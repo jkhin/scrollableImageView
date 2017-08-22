6c859fc97add96f4429b20208b4d2a142afdd644
Joel Colmenares <jkhin.cb@gmail.com>
2017-08-22 14:35:19 -0800

    stable version (base) cleaned

--- app/src/main/java/com/example/joek/parallaxeffect/MainActivity.java
+++ app/src/main/java/com/example/joek/parallaxeffect/MainActivity.java
@@ -7,10 +7,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -18,40 +16,18 @@ import android.widget.ProgressBar;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView layerLvl1, layerLvl2;
     private static final String DEBUG_TAG = "Training";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        layerLvl1 = new ScrollableImageView(this, 60);
-        layerLvl1.setLayoutParams(new LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, layerLvl1.getHeight()));
 
-        layerLvl2 = new ScrollableImageView(this, 20);
-        layerLvl2.setLayoutParams(new LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, layerLvl2.getHeight()));
-
-//        new BitmapLoaderTask().execute("asset_one.jpg", "asset_two.png");
         new BitmapLoaderTask().execute("asset_two.png");
-
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (layerLvl1 != null) {
-            switch (event.getAction()){
-                case MotionEvent.ACTION_MOVE:
-//                    layerLvl1.dispatchTouchEvent(event);
-//                    layerLvl2.dispatchTouchEvent(event);
-                    break;
-            }
-        }
-
-        return super.onTouchEvent(event);
-    }
-
-    private void setImageBitmap(Bitmap bmp) {
-        ImageView imageView = new ScrollableImageView(this, 10);
+    private void setImageBitmap(Bitmap bmp, int vel) {
+        ImageView imageView = new ScrollableImageView(this, vel);
         imageView.setLayoutParams(new LinearLayout.LayoutParams(bmp.getWidth(), bmp.getHeight()));
         imageView.setImageBitmap(bmp);
         ViewGroup container = (ViewGroup) findViewById(R.id.container);
@@ -92,8 +68,7 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(result);
             progress.setVisibility(View.INVISIBLE);
 
-            setImageBitmap(result[0]);
-//            setImageBitmap(result[1], layerLvl2);
+            setImageBitmap(result[0], 20);
         }
 
     }
